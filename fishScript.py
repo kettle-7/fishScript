15a54fc7786821f28b7b8a52dfb8e7d05a946b7a
Kettle3D <64388266+Kettle3D@users.noreply.github.com>
2020-05-21 13:18:02 +1200

    Finished? the wrapper for the time module.

--- fishScript.py
+++ fishScript.py
@@ -34,13 +34,19 @@ def parseRawInt(string=''):
         pass
     return int(float(output))
 
-# Everything below this line is an experiment I'm working on
+# Everything below this line is an experiment I'm working on: https://github.com/Kettle3D/fishScript
 
 v_defs = ['$n', '$s', '$f', '$i', '$a', '$t', '$d', '$b', '$x', '$o', '$c', '$l', '$m']
-from os import getenv
+from os import getenv, getcwd
+from os.path import normpath
 from sys import platform
+import copy
+import os
+
 if platform.startswith('win32') or platform.startswith('cygwin'):
     libdir = getenv('appdata') + "\\fishScript\\wrapper\\"
+
+path = [getcwd() + normpath('/'), libdir]
 
 def delPartOfString(script, index=0):
     output = ''
@@ -77,15 +83,38 @@ def findLines(script='', char=';', rep='\n'):
             output = output + script[p]
     return output
 
-def subVariables(script=[]):
+def subVariables(script=[], vsym='$'):
     for p in range(0, len(script)):
-        if script[p].startswith('$'):
-            if not script[p] in v_defs:
-                try:
-                    script[p] = '"%s"' % _varmap[delPartOfString(script[p])]
-                except KeyError:
-                    print("Name Error: Variable '%s' is not defined." % script[p])
+        try:
+            if script[p].startswith(vsym):
+                if not script[p] in v_defs:
+                    try:
+                        script[p] = '"%s"' % _varmap[delPartOfString(script[p])]
+                    except KeyError:
+                        print("Name Error: Variable '%s' is not defined." % script[p])
+        except AttributeError:
+            for pl in range(0, len(script[p])):
+                if script[p][pl].startswith(vsym):
+                    if not script[p][pl] in v_defs:
+                        try:
+                            script[p][pl] = '%s' % _varmap[delPartOfString(script[p][pl])]
+                        except KeyError:
+                            print("Name Error: Variable '%s' is not defined." % script[p][pl])
     return script
+
+def addListItems(script=[]):
+    sl = ''
+    try:
+        for p in range(0, len(script)):
+            if sl == '':
+                sl = script[p]
+            else:
+                sl = sl + ' ' + script[p]
+    except TypeError:
+        sl = []
+        for p in range(0, len(script)):
+            sl = sl + script[p]
+    return sl
 
 _varmap = {}
 _vararray = []
@@ -112,6 +141,9 @@ def fishScriptLine(script, args=()):
         return
     if script[0] == 'log':
         sl = ''
+        if len(script) == 1:
+            print("Command Error: You need to provide parameters for this function.")
+            return
         for kw in script[1:len(script)]:
             if sl == '':
                 sl = kw
@@ -135,7 +167,7 @@ def fishScriptLine(script, args=()):
             else:
                 sl = sl + ' ' + kw
             pass
-        exec(sl)
+        exec(subVariables(sl))
         pass
     elif script[0] == '$s' or script[0] == 'string':
         sl = ''
@@ -155,6 +187,59 @@ def fishScriptLine(script, args=()):
                     print("Name Error: Variable '%s' is not defined." % script[1])
         except IndexError:
             print('Syntax Error: invalid syntax for command $s')
+        pass
+    elif script[0] == '-s' or script[0] == 'Pstring':
+        sl = ''
+        for kw in script[2:len(script)]:
+            if not sl == '':
+                sl = sl + ' ' + kw
+            else:
+                sl = kw
+            pass
+        try:
+            if not script[2].startswith('-'):
+                _varmap[script[1]] = sl
+            else:
+                try:
+                    _varmap[script[1]] = _varmap[delPartOfString(script[2])]
+                except KeyError:
+                    print("Name Error: Variable '%s' is not defined." % script[1])
+        except IndexError:
+            print('Syntax Error: invalid syntax for command -s')
+        pass
+    elif script[0] == '$p' or script[0] == '$@':
+        sl = ''
+        for kw in script[2:len(script)]:
+            if not sl == '':
+                sl = sl + ' ' + kw
+            else:
+                sl = kw
+            pass
+        try:
+            if not script[2].startswith('$'):
+                _varmap[script[1]] = exec(sl)
+            else:
+                try:
+                    _varmap[script[1]] = _varmap[delPartOfString(script[2])]
+                except KeyError:
+                    print("Name Error: Variable '%s' is not defined." % script[1])
+        except IndexError:
+            print('Syntax Error: invalid syntax for command $p')
+        pass
+    elif script[0] == 'include':
+        sl = ''
+        for location in path:
+            if os.path.exists(location + script[1] + '.fs'):
+                file = open(location + script[1] + '.fs')
+                sl = file.read()
+                file.close()
+                break
+                pass
+            pass
+        if '' == sl:
+            print("File Error: File %s%s couldn't be found." % (path[0], script[1] + '.fs'))
+        else:
+            fishScript(sl)
         pass
     elif script[0] == '$i' or script[0] == 'int':
         _varname = script[1]
@@ -203,16 +288,48 @@ def fishScriptLine(script, args=()):
         del script[1]
         _varmap[_varname] = None
         pass
-    elif script[0] == '$m' or script[0] == 'function' or script[0] == 'method':
+    elif script[0] == '$m' or script[0] == 'function':
         _varname = script[1]
         del script[0:2]
+        if not 'does' in script:
+            print("Your script does not contain the 'does' keyword. Use it to separate the parameters from the script.")
+            return
+        parameters = subVariables(replaceWord(script, word='does'))
+        _code = replaceWord(script, start='does')
         code = ''
-        for kw in script:
+        for kw in _code:
             if code == '':
                 code = kw
             else:
                 code = code + ' ' + kw
-        _varmap[_varname] = 'fishScript("exe %s")' % code
+        ye = ''
+        _varmap[_varname] = {
+            "parameters" : parameters,
+            "code" : code,
+            "type" : "method"
+        } #'fishScript("exe %s")' % code
+        _vararray.append(_varname)
+    elif  script[0] == 'method':
+        _varname = script[1]
+        del script[0:2]
+        if not 'does' in script:
+            print("Your script does not contain the 'does' keyword. Use it to separate the parameters from the script.")
+            return
+        parameters = subVariables(replaceWord(script, word='does'))
+        del parameters[0]
+        _code = replaceWord(script, start='does')
+        code = ''
+        for kw in _code:
+            if code == '':
+                code = kw
+            else:
+                code = code + ' ' + kw
+        ye = ''
+        _varmap[_varname] = {
+            "parameters" : parameters,
+            "code" : code,
+            "type" : "raw"
+        } #'fishScript("exe %s")' % code
         _vararray.append(_varname)
     elif script[0] == 'exe':
         del script[0]
@@ -222,7 +339,7 @@ def fishScriptLine(script, args=()):
                 sl = kw
             else:
                 sl = sl + ' ' + kw
-        fishScript(findLines(sl))
+        fishScript(subVariables(findLines(sl)))
     elif script[0] == 'if':
         con = subVariables(replaceWord(script))
         _code = replaceWord(script, start='then')
@@ -239,12 +356,33 @@ def fishScriptLine(script, args=()):
             else:
                 ye = ye + ' ' + kw
         eval(ye + 'fishScript("exe %s")' % code)
-    elif script[0] == '//' or script[0] == '#':
+    elif script[0] == '//':
         pass
     elif script[0] in _vararray:
-        exec(_varmap[script[0]])
+        funcname = script[0]
+        del script[0]
+        paras = _varmap[funcname]["parameters"]
+        for p in range(0, len(script)):
+            try:
+                paras[p] = script[p]
+            except IndexError:
+                print("Command Error: Call to function was missing parameters (%s required, %s given)" % (len(paras), len(script)))
+                return
+        for p in range(0, len(paras)):
+            try:
+                fishScript('-s %s %s' % (paras[p], script[p]))
+            except IndexError:
+                print("Command Error: Call to function was missing parameters (%s required, %s given)" % (len(paras), len(script)))
+                return
+        if _varmap[funcname]["type"] == "method":
+            lins = "exe %s" % _varmap[funcname]["code"]
+        else:
+            lins = _varmap[funcname]["code"]
+        lin = subVariables(lins.split())
+        li = addListItems(lin)
+        fishScript(li)
     else:
-        print("CommandError: Unknown command: %s" % script[0])
+        print("Command Error: Unknown command: %s" % script[0])
     pass
 
 fishScript()
